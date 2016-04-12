786c332e3f3dc13a297cd38fba06e537f1377394
giri-sandeep <girisandeep@hotmail.com>
2016-04-12 07:54:43 +0200

    SST25VF040B
    This program read/write data from/to the SST25VF040B external flash
    memory.

---
+++ SST25VF040B.X/SST25VF040B.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   SST25VF040B.h
+ * Author: giri
+ *
+ * Created on 20. August 2014, 15:36
+ */
+
+#ifndef SST25VF040B_H
+#define	SST25VF040B_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SST25VF040B_H */
+
---
+++ SST25VF040B.X/build/default/production/SST25VF040B.o.d
@@ -0,0 +1,2 @@
+build/default/production/SST25VF040B.o:  \
+SST25VF040B.c 
